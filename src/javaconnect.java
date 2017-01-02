b632862f9595e7b9e43ee786e4c9bacacdeffa5e
shoaibtech90 <shoaib.tech90@gmail.com>
2017-01-03 03:36:08 +0530

    Employees Management System
    Major Project(Still working On...)

---
+++ src/javaconnect.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author SHOAIB
+ */
+import java.sql.*;
+import javax.swing.*;
+
+public class javaconnect {
+
+    Connection conn = null;
+    public static Connection ConnecrDb()
+    {
+        try
+        {
+            Class.forName("org.sqlite.JDBC");
+            Connection conn = DriverManager.getConnection("JDBC:sqlite:C:\\Users\\SHOAIB\\Documents\\NetBeansProjects\\mainjavaproject\\javamainproject.sqlite");
+            JOptionPane.showMessageDialog(null, "Connection Established");
+            return conn;
+            
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, e);
+            return null;
+        }
+    }
+    
+    
+    
+    
+}
